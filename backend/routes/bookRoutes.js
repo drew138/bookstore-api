a7160bd138632ce3141a7ec376eaab7b2eca40ea
Andres Salazar Galeano <58788781+drew138@users.noreply.github.com>
2024-10-30 14:21:47 -0500

    fix: wip

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -8,9 +8,8 @@ import {
 const router = express.Router();
 
 router.route("/").get(getBooks);
-router.route("/sendEmail").get(sendBooksEmail);
+router.route("/sendEmail").post(sendBooksEmail);
 
 router.route("/:id").get(getBooksById);
 
 export default router;
-
